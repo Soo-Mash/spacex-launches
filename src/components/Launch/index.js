255a21d4b5535acd959b7cf0bec792e3245e34cb
SooMash <sigavjones@gmail.com>
2023-08-10 09:58:49 +0100

    installed prettier

--- src/components/Launch/index.js
+++ src/components/Launch/index.js
@@ -1,17 +1,15 @@
-import styles from './launch.module.css'
-import moment from 'moment'
+import styles from './launch.module.css';
+import moment from 'moment';
 
-const Launch = ({props}) => {
+const Launch = ({ props }) => {
+   return (
+      <div>
+         <div className={styles.launchTitle}>{props.name}</div>
+         <div>Id: {props.id}</div>
+         <div>Flight Number: {props.flight_number}</div>
+         <div>Launch Date: {moment(props.date_utc).format('MMMM Do YYYY, h:mm:ss a')}</div>
+      </div>
+   );
+};
 
-    return (
-        <div>
-            <div className={styles.launchTitle}>{props.name}</div>
-            <div>Id: {props.id}</div>
-            <div>Flight Number: {props.flight_number}</div>
-            <div>Launch Date: {moment(props.date_utc).format('MMMM Do YYYY, h:mm:ss a')}</div>
-        </div>
-    )
-
-}
-
-export default Launch
+export default Launch;
--- src/components/Launches/index.js
+++ src/components/Launches/index.js
@@ -1,20 +1,19 @@
-import {Grid, Paper} from "@mui/material";
-import Launch from "../Launch";
+import { Grid, Paper } from '@mui/material';
+import Launch from '../Launch';
 import styles from './launches.module.css';
 
-const Launches = ({launches}) =>  {
-    return (
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-            {launches.map((launch) => (
-                <Grid key={launch.id} item xs={4} lg={4}>
-                    <Paper className={styles.page}>
-                        <Launch props={launch}/>
-                    </Paper>
-                </Grid>
-            ))}
-        </Grid>
-    );
-}
+const Launches = ({ launches }) => {
+   return (
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+         {launches.map((launch) => (
+            <Grid key={launch.id} item xs={4} lg={4}>
+               <Paper className={styles.page}>
+                  <Launch props={launch} />
+               </Paper>
+            </Grid>
+         ))}
+      </Grid>
+   );
+};
 
-
-export default Launches
+export default Launches;
